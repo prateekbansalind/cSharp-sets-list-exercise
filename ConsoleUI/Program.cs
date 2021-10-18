25fec18ad3a014145302a95fccc6e69718a6f89a
pbansal16 <prateekbansalind@gmail.com>
2021-10-18 16:13:42 +1300

    Initial Commit : Program to demonstrate the real life use of list data type.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add students to a class roaster until there are no more students.
+            //Then print out the count of the students to the console.
+            List<string> firstNames = new List<string>();
+            string firstName = "";
+            string decisionVar = "";
+
+            do
+            {
+                Console.Write("Enter you name (student first name): ");
+                firstName = Console.ReadLine();
+
+                firstNames.Add(firstName);
+
+                Console.Write("Do you want to make another entry (y/n): ");
+                decisionVar = Console.ReadLine(); 
+
+            } while (decisionVar.ToLower() == "y");
+
+            Console.WriteLine($"The number of student already saved in the database are {firstNames.Count}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
